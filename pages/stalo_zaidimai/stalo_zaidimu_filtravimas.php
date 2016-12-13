18a8bb951c7d628155992692e5df26eae917df80
Paulinik <paulius.pat@gmail.com>
2016-12-13 22:50:24 +0200

    baigtas filtravimas

--- pages/stalo_zaidimai/stalo_zaidimu_filtravimas.php
+++ pages/stalo_zaidimai/stalo_zaidimu_filtravimas.php
@@ -1,28 +1,82 @@
-<div class="row">
-  <div class="large-6 columns">
-    <label for="start">Start Date</label>
-    <input id="start" type="date" /><br />
-  </div>
-  <div class="large-6 columns">
-    <label for="end">End Date</label>
-    <input id="end" type="date" /><br />
-  </div>
-</div>
-<div class="row">
-  <div class="large-12 columns">
-    <button class="button radius" id="filter">Filter</button>
-    <button id="clearFilter" class="button radius secondary">Clear Filter</button>
-  </div>
-</div>
-<div class="row">
-  <div class="large-12 columns">
-    <table id="oTable">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Date</th>
-        </tr>
-      </thead>
-    </table>
-  </div>
-</div>
+<?php
+include 'dbh.php';
+$adresas = "/Boardy/index.php?module=stalo_zaidimu_perziura";
+if(isset($_POST['filtravimas']))
+{
+    if (($_POST["age"])!="-") {
+        $adresas = $adresas.''."&amzius=".''.$_POST["age"];
+    }
+    if (($_POST["amount"])!="-") {
+        $adresas = $adresas.''."&kiekis=".''.$_POST["amount"];
+    }
+    if (($_POST["genre"])!="-") {
+        $adresas = $adresas.''."&zanras=".''.$_POST["genre"];
+    }
+    if (($_POST["length"])!="-") {
+        $adresas = $adresas.''."&trukme=".''.$_POST["length"];
+    }
+    header('Location: '. $adresas);
+}
+?>
+<form class="form-horizontal" method="post" enctype="multipart/form-data"> 
+    <fieldset>
+        <div class="col-md-5">
+            <div class="form-group">
+                <label for="age" class="col-lg-7 control-label">Žaidėjų amžius</label>
+                    <div class="col-lg-4">
+                        <select class="form-control" id="age" name="age">
+                          <option selected>-</option>
+                          <option>3+</option>
+                          <option>7+</option>
+                          <option>10+</option>
+                          <option>12+</option>
+                          <option>18+</option>
+                        </select>
+                    </div>
+                <label for="amount" class="col-lg-7 control-label">Žaidėjų kiekis</label>
+                    <div class="col-lg-4">
+                        <select class="form-control" id="amount" name="amount">
+                          <option selected>-</option>
+                          <option>2+</option>
+                          <option>3+</option>
+                          <option>4+</option>
+                        </select>
+                    </div>
+            </div>
+        </div>
+        
+        <div class="col-md-5">
+            <div class="form-group">
+                <label for="genre" class="col-lg-7 control-label">Žanras</label>
+                    <div class="col-lg-4">
+                        <?php
+                            $sqlI = "SELECT id, pavadinimas FROM Zaidimo_kategorija"; 
+                            $resultI = mysqli_query($conn, $sqlI);
+                          ?>
+                        <select class="form-control" id="genre" name="genre">
+                          <option selected>-</option>
+                          <?php
+                            while($row = mysqli_fetch_array($resultI)) {
+                                echo'<option value='.$row[id].'>'.$row[pavadinimas].'</option>';
+                            }
+                          ?> 
+                        </select>
+                    </div>
+                <label for="length" class="col-lg-7 control-label">Žaidimo trukmė</label>
+                    <div class="col-lg-4">
+                        <select class="form-control" id="length" name="length">
+                          <option selected>-</option>
+                          <option><=30min</option>
+                          <option>>30min</option>
+                        </select>
+                    </div>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="form-group">
+                <input name="filtravimas" type="submit" class="btn btn-primary" value="Filtruoti">  
+            </div>
+        </div>
+        
+    </fieldset>
+</form>
--- pages/stalo_zaidimai/stalo_zaidimu_rezervacija.php
+++ pages/stalo_zaidimai/stalo_zaidimu_rezervacija.php
@@ -2,9 +2,9 @@
 include 'dbh.php';
 //pateikti visą informaciją apie žaidimą ir leisti jį rezervuoti
 if(isset($_GET['id'])) {
-$sql = "SELECT * FROM Zaidimas WHERE id=" .$_GET['id'];
-$result = mysqli_query($conn, $sql);
-$row = mysqli_fetch_array($result);
+    $sql = "SELECT * FROM Zaidimas WHERE id=" .$_GET['id'];
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_array($result);
 }
 ?>
 
@@ -66,33 +66,66 @@ $row = mysqli_fetch_array($result);
         <!-- Trigger the modal with a button -->
         <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">Rezervuoti</button>
 
-        <!-- Modal -->
-        <div class="modal fade" id="myModal" role="dialog">
-          <div class="modal-dialog">
+        <?php 
+        if(!empty($_SESSION['user']))
+        {
+            echo'
+            <!-- Modal -->
+            <div class="modal fade" id="myModal" role="dialog">
+              <div class="modal-dialog">
 
-            <!-- Modal content-->
-            <div class="modal-content">
-              <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">Žaidimo rezervavimas</h4>
-              </div>
-                
-              <div class="modal-body">
-                <form>
-                    Kada norite atsiimti žaidimą?<br>
-                    <input type="date" name="from" min="<?php date("m-d-Y");  ?>"><br><br>
-                    Kada norite grąžinti žaidimą?<br>
-                    <input type="date" name="to" min="<?php date("m-d-Y");  ?>"><br><br>
-                    <p style="text-align:justify;"> *Nuomos kaina - 1eu už dieną</p>
-                </form>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-success">Patvirtinti</button>
+                <!-- Modal content-->
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Žaidimo rezervavimas</h4>
+                  </div>
+
+                  <div class="modal-body">
+                    <form>
+                        Kada norite atsiimti žaidimą?<br>
+                        <input type="date" name="from" min="<?php date("m-d-Y");  ?>"><br><br>
+                        Kada norite grąžinti žaidimą?<br>
+                        <input type="date" name="to" min="<?php date("m-d-Y");  ?>"><br><br>
+                        <p style="text-align:justify;"> *Nuomos kaina - 1eu už dieną</p>
+                    </form>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-success">Patvirtinti</button>
+                  </div>
+                </div>
+
               </div>
             </div>
+            '; 
+        } else{
+            echo'
+            <!-- Modal -->
+            <div class="modal fade" id="myModal" role="dialog">
+              <div class="modal-dialog">
 
-          </div>
-        </div>
+                <!-- Modal content-->
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Užsiregistruokite</h4>
+                  </div>
+
+                  <div class="modal-body">
+                    <form>
+                        <p style="text-align:justify;"> Reikalinga registracija</p>
+                    </form>
+                  </div>
+                  <div class="modal-footer">
+                    <a href="index.php?module=kliento_profilio_kurimas" type="button" class="btn btn-success">Registruotis</a>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+            ';
+        }
+        ?>
   
 </div>
         
